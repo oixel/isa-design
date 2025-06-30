d04068403632fb60bb03e68d938fd402e4bbc351
Oixel Romero <oycell@aol.com>
2025-06-30 12:23:33 -0400

    Create basic navbar and pages

---
+++ src/lib/components/DottedBackground.svelte
@@ -0,0 +1,29 @@
+<div id="background"></div>
+
+<style>
+    @keyframes animatedBackground {
+        from {
+            background-position: 0 0;
+        }
+        to {
+            background-position: top 400px left 400px;
+        }
+    }
+
+    #background {
+        background: white;
+        background-image: radial-gradient(black 2px, transparent 0);
+        background-size: 40px 40px;
+        opacity: 8%;
+
+        position: fixed;
+
+        height: 100vh;
+        width: 100vw;
+        z-index: -100;
+        padding: 0;
+        margin: 0;
+
+        animation: animatedBackground 80s linear infinite;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,69 @@
 <script lang="ts">
 	import '../app.css';
+	
+	import DottedBackground from '$lib/components/DottedBackground.svelte';
+
+	import { goto } from '$app/navigation';
 
 	let { children } = $props();
+
+	let navbarHeight = $state(0);
 </script>
 
-{@render children()}
+<!-- This is the NavBar -->
+<div bind:offsetHeight={navbarHeight} class="fixed w-[100vw] border-b-2 p-5 flex gap-5 bg-white">
+	<!-- Name / portfolio button -->
+	<button 
+		onclick={() => goto("/")}
+		class="figtree text-2xl hover:cursor-pointer hover:scale-102 transition-transform duration-100">
+		Isabela Delgado
+	</button>
+	
+	<!-- Add little divider icon to separate name from section buttons -->
+	<p class="figtree pt-1 select-none">
+		~
+	</p>
+
+	<!-- Define a small snippet for the different section buttons to avoid redundant code -->
+	{#snippet sectionButton(title : string)}
+		<button
+			onclick={() => goto(`/${title}`)}
+			class="figtree hover:border-b-2 hover:cursor-pointer"
+		>
+			{title}
+		</button>
+	{/snippet}
+	
+	<!-- Create snippets for each of the section buttons -->
+	{@render sectionButton("about")}
+	{@render sectionButton("personal")}
+	{@render sectionButton("contact")}
+</div>
+
+<div class="flex flex-col">
+	<!-- Places an invisible div with the same dimensions as navbar to offset page content properly -->
+	<div class="bg-red-500" style="height: {navbarHeight}px;"></div>
+	
+	<!-- Render out page content! -->
+	{@render children()}
+</div>
+
+<DottedBackground />
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&family=Lora:ital,wght@0,400..700;1,400..700&display=swap');
+
+	:global {
+		.lora {
+			font-family: "Lora", sans-serif;
+			font-optical-sizing: auto;
+			font-style: normal;
+		}
+
+		.figtree {
+			font-family: "Figtree", sans-serif;
+			font-optical-sizing: auto;
+			font-style: normal;	
+		}
+	}
+</style>
