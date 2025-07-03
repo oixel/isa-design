e9395a749b28b761ecd6d35732f3421c0b5e19f8
Oixel Romero <oycell@aol.com>
2025-07-03 11:54:35 -0400

    Fix scaling

--- src/lib/components/WIP.svelte
+++ src/lib/components/WIP.svelte
@@ -2,8 +2,11 @@
     import { scale } from "svelte/transition";
 </script>
 
-<div in:scale class='grow w-1/3 aspect-video m-auto bg-yellow-200 border-2 flex'>
-    <div class='grow m-5 bg-white border-2 border-dashed flex justify-center items-center'>
-        <h1 class="text-2xl">🚧 This page is still under construction! 🚧</h1>
+
+<div class="w-full h-full flex justify-center">
+    <div in:scale class='grow w-full max-w-200 aspect-video m-auto bg-yellow-200 border-2 flex'>
+        <div class='grow m-5 bg-white border-2 border-dashed flex justify-center items-center'>
+            <h1 class="text-sm sm:text-2xl">🚧 This page is still under construction! 🚧</h1>
+        </div>
     </div>
 </div>
