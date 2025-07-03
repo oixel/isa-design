57df6bff2d3f2050c2755e85a0123225cf394860
Oixel Romero <oycell@aol.com>
2025-07-03 12:06:14 -0400

    Ensure that exit button remains square

--- src/lib/components/ExitButton.svelte
+++ src/lib/components/ExitButton.svelte
@@ -5,7 +5,7 @@
 <button
 	{onclick}
 	class="{position} flex w-9 sm:w-12 cursor-pointer items-center justify-center 
-    border-2 bg-white p-3 outline-0 
+    border-2 bg-white p-3 outline-0 aspect-square 
     transition-all duration-100 hover:scale-105 hover:border-dashed focus:scale-105 focus:border-dashed active:scale-105 active:border-dashed"
 >
 	X
