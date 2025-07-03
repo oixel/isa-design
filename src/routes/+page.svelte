729cb11714fb8ecbf4d2d9d46843b738c6e4db25
Oixel Romero <oycell@aol.com>
2025-07-03 09:09:15 -0400

    Add option to toggle between list and grid on large screens

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,8 @@
 	import PortfolioTile from '$lib/components/PortfolioTile.svelte';
 
 	let { data } = $props()
+
+	let isList = $state(true);
 </script>
 
 <svelte:head>
@@ -10,8 +12,32 @@
 	</title>
 </svelte:head>
 
-<div class="flex h-full flex-row justify-center items-center gap-10 flex-wrap">
-	{#each data.projects as project}
-		<PortfolioTile {...project} />
-	{/each}
+
+<div class="figtree flex flex-col justify-center">
+	<!-- Adds options to toggle between list and grid on large screen -->
+	<div class="w-9/10 mx-auto flex justify-end mb-8 gap-6">
+		<div class="hidden lg:visible lg:flex w-25 py-2 border-2 gap-2 justify-center items-center bg-white">
+			<button onclick={() => { isList = true; }} class="{(isList) ? 'border-b-2' : ''} hover:border-b-2 cursor-pointer">list</button>
+			<button onclick={() => { isList = false; }} class="{(!isList) ? 'border-b-2' : ''} hover:border-b-2 cursor-pointer">grid</button>
+		</div>
+	</div>
+
+	<!-- Renders portfolio projects as a list or grid (grid only allowed on large devices) -->
+	<div class="list {(isList) ? 'list' : 'grid'}">
+		{#each data.projects as project}
+			<PortfolioTile {...project} />
+		{/each}
+	</div>
 </div>
+
+<style>
+	@import 'tailwindcss';
+
+	.list {
+		@apply flex h-full flex-row justify-center items-center gap-10 flex-wrap;
+	}
+
+	.grid {
+		@apply lg:grid lg:grid-cols-3 lg:justify-items-center;
+	}
+</style>
