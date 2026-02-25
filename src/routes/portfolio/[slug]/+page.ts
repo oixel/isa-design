de811ef2effc59ab05df0034412f0fffd8a637f6
Oixel Romero <oycell@aol.com>
2026-02-24 19:12:15 -0500

    add entries for slug page

--- src/routes/portfolio/[slug]/+page.ts
+++ src/routes/portfolio/[slug]/+page.ts
@@ -1,22 +1,33 @@
 import type { MdsvexFile } from '$lib/types/MdsvexFile.js';
 import { error } from '@sveltejs/kit';
 
+export const prerender = true;
+
+export function entries() {
+    return [
+        { slug: 'big-building' },
+        { slug: 'floridalandscape' },
+        { slug: 'towers' },
+        // Add names of all projects in src/projects/*.md here
+    ];
+}
+
 // 
 function findMatch(pageSlug: string, paths: Record<string, () => Promise<unknown>>) {
     for (const [path, resolver] of Object.entries(paths)) {
         const entrySlug = path.split('/').at(-1)?.replace('.md', '');
         if (entrySlug == pageSlug) {
-            return { 
-                path, 
-                resolver: resolver as unknown as Promise<MdsvexFile> 
-            }; 
+            return {
+                path,
+                resolver: resolver as unknown as Promise<MdsvexFile>
+            };
         }
     }
 }
 
-export async function load ({ params }) {
+export async function load({ params }) {
     const paths = import.meta.glob("/src/projects/*.md");
-    
+
     const match = findMatch(params.slug, paths);
 
     const project = await match?.resolver?.();
